e5df43807695b8188999b7e11e9cca0a504ff4cd
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-03-27 23:22:41 +0530

    updated

--- slcm_captcha.py
+++ slcm_captcha.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup
 import time
 
 BASE_URL = "https://slcm.manipal.edu/"
-header = {"user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'}
+header = {
+    "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'}
 
 for i in range(100):
-    
-    s = requests.session()
-    page = s.get(BASE_URL,headers = header)
-    soup = BeautifulSoup(page.content,'lxml')
 
-    img = (soup.find('img',id = "imgCaptcha"))
+    s = requests.session()
+    page = s.get(BASE_URL, headers=header)
+    soup = BeautifulSoup(page.content, 'lxml')
+
+    img = (soup.find('img', id="imgCaptcha"))
 
     final_url = BASE_URL+str(img.get('src'))
     print(final_url)
